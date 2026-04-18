dbc887526a538c499ef04ba0e21379e7fbf05b74
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2026-04-18 18:35:49 +0500

    chore: Move Docker and gitignore files to project root

---
+++ FileIntegrityChecker/Enums/ScanType.cs
@@ -0,0 +1,12 @@
+namespace FileIntegrityChecker.Enums;
+
+/// <summary>
+/// Defines the types of scans that can be performed.
+/// </summary>
+// OOP: Enum (Provides discrete set of values, improving type safety)
+public enum ScanType
+{
+    Quick,
+    Deep,
+    Baseline
+}
